0d7c9e1a087074f41ccae4a2bdc26ba2e908ae14
Alex han <s302713@naver.com>
2022-04-12 15:12:39 +0900

    aaa

--- src/Com.java
+++ src/Com.java
@@ -1,14 +1,20 @@
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Scanner;
 
 class A{
 	static int num;
 	static String name, snm, con, sm;
 }
-public class Com {
 
+
+public class Com extends A{
+ 
 	public static void main(String[] args) {
+		
+		ArrayList<A> arr = new ArrayList<A>();
+		
+		
+		
 		Scanner input = new Scanner(System.in);
 
 		for(;;) {
@@ -20,8 +26,28 @@ public class Com {
 				
 				break;
 			case 2:
+				String addr = null;
+				int age = 0;
+				String stNum = null;
 				
+				System.out.println("직원이름 입력");
+				name = input.next();
+				A n = new A();
+				n.name="홍길동";
+				n.addr="";
+				n.age="홍길동";
+				n.stNum="홍길동";
+				
+				arr.add(n);
 				break;
+		{
+				
+				
+				
+				
+				
+				
+				
 			case 3 :
 				
 				break;
