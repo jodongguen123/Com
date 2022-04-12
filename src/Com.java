f297ed754673df2b221e19968e06a5cf0480ab55
JoDongGeun <jokun2006@naver.com>
2022-04-12 14:36:31 +0900

    공통 추가

---
+++ src/Com.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+class A{
+	static int num;
+	static String name, snm, con, sm;
+}
+public class Com {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		for(;;) {
+			System.out.println("1. 학생관리  2. 직원관리  3. 게시판 관리");
+			System.out.print("숫자를 입력 : ");
+			int num = input.nextInt();
+			switch(num) {
+			case 1: 
+				
+				break;
+			case 2:
+				
+				break;
+			case 3 :
+				
+				break;
+			
+			}
+		}
+	}
+
+}
